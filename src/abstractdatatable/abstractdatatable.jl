26e87ac2c76bfef3e6f42baa1a960b3ee950fedb
Cameron Prybol <cameron.prybol@gmail.com>
2017-03-18 00:21:01 -0700

    fix v0.5 issue

--- src/abstractdatatable/abstractdatatable.jl
+++ src/abstractdatatable/abstractdatatable.jl
@@ -663,15 +663,15 @@ unique!(dt)  # modifies dt
 """
 (unique, unique!)
 
-function nonuniquekey(dt::AbstractDataTable)		
-    # Here's another (probably a lot faster) way to do `nonunique`		
-    # by grouping on all columns. It will fail if columns cannot be		
-    # made into CategoricalVector's.		
-    gd = groupby(dt, _names(dt))		
-    idx = [1:length(gd.idx)][gd.idx][gd.starts]		
-    res = fill(true, nrow(dt))		
-    res[idx] = false		
-    res		
+function nonuniquekey(dt::AbstractDataTable)
+    # Here's another (probably a lot faster) way to do `nonunique`
+    # by grouping on all columns. It will fail if columns cannot be
+    # made into CategoricalVector's.
+    gd = groupby(dt, _names(dt))
+    idx = [1:length(gd.idx)][gd.idx][gd.starts]
+    res = fill(true, nrow(dt))
+    res[idx] = false
+    res
 end
 
 # Count the number of missing values in every column of an AbstractDataTable.
@@ -752,7 +752,8 @@ function Base.vcat(dts::AbstractDataTable...)
     if length(uniqueheaders) > 1
         if !isempty(coldiff)
             headerlengths = length.(allheaders)
-            minheaderloci = find(headerlengths .== minimum(headerlengths))
+            m = minimum(headerlengths)
+            minheaderloci = find(h -> h == m, headerlengths)
             throw(ArgumentError("column(s) ($(join(string.(coldiff), ", "))) are missing from argument(s) ($(join(string.(minheaderloci), ", ")))"))
         else
             estrings = Vector{String}(length(uniqueheaders))
